3df67ddd7e5f81fba7dbde687bcf1537f4717d7d
YD <dongdong0815@naver.com>
2022-11-14 20:26:12 +0900

    메인 네비게이션, 상세화면 페이지 추가

---
+++ app/src/main/java/com/example/snsproject/navigation/model/ContentDTO.kt
@@ -0,0 +1,13 @@
+package com.example.snsproject.navigation.model
+
+class ContentDTO (var explain : String? = null,
+                    var imageUrl : String? = null,
+                    var uid : String? = null,
+                    var userId : String? = null,
+                    var timestamp : Long? = null,
+                    var favorites : Map<String,Boolean> = HashMap()){
+        data class Comment(var uid : String?= null,
+                           var userid : String?= null,
+                           var comment : String? = null,
+                            var timestamp: Long? = null )
+    }
