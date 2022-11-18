d3effd998242583e2282f1bc2fdf1d3a2aa40f23
sunwonga <sunwonga@gmail.com>
2022-11-18 16:57:33 +0900

    이메일 유효성 검사 추가

--- app/src/main/java/com/example/snsproject/AddAccountActivity.kt
+++ app/src/main/java/com/example/snsproject/AddAccountActivity.kt
@@ -1,30 +1,64 @@
 package com.example.snsproject
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.snsproject.databinding.ActivityAddAccountBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import java.util.regex.Pattern
 
 class AddAccountActivity : AppCompatActivity() {
+
+    // 검사 정규식
+    private val emailValidation =
+        "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
+
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+
     private val binding by lazy { ActivityAddAccountBinding.inflate(layoutInflater) }
     private val auth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        emailEditText = binding.emailEditText
+        passwordEditText = binding.passwordEditText
+
+        emailEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                checkEmail()
+            }
+
+        })
+
         val emailSignInBtn = binding.emailSignInBtn
 
         emailSignInBtn.setOnClickListener {
-            signInAndUp()
+            if (!checkEmail()) {
+                Toast.makeText(this, "이메일을 형식에 맞게 입력하세요", Toast.LENGTH_SHORT).show()
+            } else if (passwordEditText.text.toString() == "") {
+                Toast.makeText(this, "패스워드 공란 ", Toast.LENGTH_SHORT).show()
+            } else {
+                signInAsEmail()
+            }
         }
     }
 
     private fun signInAndUp() {
-        val emailEditText = binding.emailEditText
-        val passwordEditText = binding.passwordEditText
         auth.createUserWithEmailAndPassword(
             emailEditText.text.toString(),
             passwordEditText.text.toString()
@@ -43,8 +77,6 @@ class AddAccountActivity : AppCompatActivity() {
     }
 
     private fun signInAsEmail() {
-        val emailEditText = binding.emailEditText
-        val passwordEditText = binding.passwordEditText
         auth.signInWithEmailAndPassword(
             emailEditText.text.toString(),
             passwordEditText.text.toString()
@@ -63,6 +95,19 @@ class AddAccountActivity : AppCompatActivity() {
         if (user != null) {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
+        }
+    }
+
+    fun checkEmail(): Boolean {
+        val email = emailEditText.text.toString().trim() //공백제거
+        val p = Pattern.matches(emailValidation, email) // 서로 패턴이 맞닝?
+        return if (p) {
+            //이메일 형태가 정상일 경우
+            emailEditText.setTextColor(Color.BLACK)
+            true
+        } else {
+            emailEditText.setTextColor(Color.RED)
+            false
         }
     }
 }
